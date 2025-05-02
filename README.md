1629d0f2f87e1475ef20a64e5012aabf945e033b
David Figueroa <dgfigueroa29@gmail.com>
2025-05-02 12:38:40 -0300

    Setup base libs, folders and resources

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# CitySeeker
-With this app you will be able to search among more than 200 cities in the world, locate them on a map and choose your favorites.
+# CitySeeker by Boa Apps
+
+With this app you will be able to search among more than 200 cities in the world, locate them on a
+map and choose your favorites. It is about Android App Technical Testing with Kotlin, Compose, Ktor
+and Hilt.
+
+## Features
+
+- Make a GET request to gist.githubusercontent.com to get test data.
+- Parse the JSON response and map it to Kotlin objects.
+- Create a UI with Jetpack Compose that displays the data in a list.
+- Implement MVVM architecture to structure the code appropriately.
+- Manage UI states (loading, success, error) efficiently.
+- Use of Kotlin coroutines and reactive programming principles.
+- Proper handling of states and errors in the UI.
+- Following clean architecture and best practices in Android.
+- Searching and filtering places using a text input.
+
+### Using gist.githubusercontent.com API RESTful
+
+#### Specifically the route GET all cities
+
+```cURL
+curl --location --globoff 'https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json'
+```
+
+#### Usage/Examples
+
+```json
+[
+  {
+    "country": "AR",
+    "name": "Provincia de Mendoza",
+    "_id": 3844419,
+    "coord": {
+      "lon": -68.5,
+      "lat": -34.5
+    }
+  }
+]
+```
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,113 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.detekt)
+    kotlin("plugin.serialization") version (libs.versions.kotlin)
+}
+
+android {
+    namespace = "com.boa.test.city.seeker"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.boa.test.city.seeker"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+        viewBinding = true
+    }
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += listOf(
+                "META-INF/LICENSE.md",
+                "META-INF/LICENSE-notice.md",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE.txt",
+                "META-INF/DEPENDENCIES",
+                "META-INF/ASL2.0",
+                "META-INF/AL2.0",
+                "META-INF/LGPL2.1"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.jakewharton.timber)
+    implementation(libs.androidx.core.ktx)
+
+    //DI - Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    ksp(libs.hilt.compiler)
+
+    // Coroutines
+    implementation(libs.jetbrains.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    //UI
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.navigation.compose)
+
+    //Data
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.ktor.client.serialization.json)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.realm.base)
+
+    //Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    //Debugging
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.ui.tooling.preview)
+}
